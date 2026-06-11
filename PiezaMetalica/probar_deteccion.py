e9add909c4b1bec6253f4a7d4d2846050e682ed5
marcooramoslana-dev <marcooramoslana@gmail.com>
2026-06-11 13:00:47 -0600

    elimine comentarios

--- PiezaMetalica/probar_deteccion.py
+++ PiezaMetalica/probar_deteccion.py
@@ -1,12 +1,9 @@
 import cv2
 from ultralytics import YOLO
 
-# 1. Ajustamos la ruta exacta basada en tu nueva organización de carpetas
-# Usamos 'best.pt' apuntando directamente a la subcarpeta PiezaMetalica
 path_modelo = r"C:\Users\polic\OneDrive\Escritorio\Trabajo\CONTROL-DE-ROBOT-HANS\PiezaMetalica\best.pt"
 model = YOLO(path_modelo)
 
-# 2. Inicializar la cámara web (Usa el Python 3.13 de tu entorno de trabajo)
 cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
@@ -22,20 +19,15 @@ while True:
         print("Error: No se pudo leer el cuadro de la cámara.")
         break
 
-    # 3. Inferencia de YOLOv8 optimizada
     results = model(frame, stream=True)
 
-    # Inicializamos frame_dibujado con el cuadro original por si no detecta nada
     frame_dibujado = frame
 
-    # 4. Dibujar las cajas sobre el frame
     for r in results:
         frame_dibujado = r.plot()
 
-    # 5. Mostrar la ventana con el video local
     cv2.imshow('Deteccion de Pieza Metalica - Cobot HANS', frame_dibujado)
 
-    # 6. Salida limpia con la tecla 'q'
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
