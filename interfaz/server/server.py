0af1292cba6d56e5084373b24fc14d37b143489c
marcooramoslana-dev <marcooramoslana@gmail.com>
2026-07-10 12:21:27 -0600

    error de servidor

--- interfaz/server/server.py
+++ interfaz/server/server.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 CORS(app)  
 
 # Configuración de la base de datos
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///robot_monitoreo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../robot_monitoreo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
